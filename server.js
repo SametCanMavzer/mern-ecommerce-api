c311a85b5b3bba7cc46ca2dd8e79e5763c084a93
Samet Can Mavzer <sametmavzer6@gmail.com>
2024-11-14 23:04:21 +0300

    Database connection provide

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./src/routes/auth.route.js";
+import { connectDB } from "./src/lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,5 +13,7 @@ app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on http://localhost:${process.env.PORT}`);
+	connectDB();
 });
+
 
