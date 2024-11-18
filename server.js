b0817c8ecd858d8126782d0accdbafc31e453823
Samet Can Mavzer <sametmavzer6@gmail.com>
2024-11-18 18:04:24 +0300

    auth middleware was created and product routes were started

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./src/routes/auth.route.js";
 import { connectDB } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
+
+// Routes
+import authRoutes from "./src/routes/auth.route.js";
+import productRoutes from "./src/routes/product.route.js";
 
 dotenv.config();
 
@@ -13,6 +16,7 @@ app.use(express.json()); // Parse JSON bodies
 app.use(cookieParser()); // Parse cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on http://localhost:${process.env.PORT}`);
