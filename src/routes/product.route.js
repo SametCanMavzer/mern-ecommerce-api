e95e51336e513607fcdfff03a094a7569fb9e234
Samet Can Mavzer <sametmavzer6@gmail.com>
2024-11-19 23:18:55 +0300

    Added Redis caching for featured products and integrated Cloudinary for product creation.

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { createProduct, getAllProducts } from "../controllers/product.controller.js";
 const router = express.Router();
 
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
 
 
 export default router;
