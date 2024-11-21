8114968ec192c01b8367e06d07afaf2e0c0fa609
Samet Can Mavzer <sametmavzer6@gmail.com>
2024-11-21 18:42:22 +0300

    Finalized all product routes, including CRUD operations and additional features

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-import { createProduct, getAllProducts } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts, getProductsByCategory, getRecommendedProducts, toggleFeaturedProduct } from "../controllers/product.controller.js";
 const router = express.Router();
 
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/recommendations", getRecommendedProducts);
+router.get("/category/:category", getProductsByCategory);
 router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 
 export default router;
