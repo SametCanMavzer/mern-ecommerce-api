01de82aa13d41db0963335836c97086e0d798f90
Samet Can Mavzer <sametmavzer6@gmail.com>
2024-12-21 22:34:35 +0300

    Set JSON body size limit to 5mb

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 
-app.use(express.json()); // Parse JSON bodies
+app.use(express.json({limit: '5mb'})); // Parse JSON bodies
 app.use(cookieParser()); // Parse cookies
 
 app.use("/api/auth", authRoutes);
